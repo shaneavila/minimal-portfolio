ccfcb7d093df112521be9906da081eae0819c35b
Shane Avila-Booth <avila.shane@yahoo.com>
2023-12-04 20:35:23 -0800

    Change imgUrl path

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Terminal Website',
-        imgUrl: 'minimal-portfolio/assets/terminal-website.jpg',
+        imgUrl: '/minimal-portfolio/assets/terminal-website.jpg',
         stack: ['HTML', 'CSS', 'JavaScript'],
         link: 'https://github.com/shaneavila/rtx-alerts',
     },
     {
         title: 'Developer Portfolio',
-        imgUrl: 'minimal-portfolio/assets/dev-portfolio.jpg',
+        imgUrl: '/minimal-portfolio/assets/dev-portfolio.jpg',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'https://github.com/shaneavila/n-queens-problem',
     },
     {
         title: 'AI Nerf Gun',
-        imgUrl: 'minimal-portfolio/assets/ai-nerf-gun.jpg',
+        imgUrl: '/minimal-portfolio/assets/ai-nerf-gun.jpg',
         stack: ['Java', 'Python'],
         link: 'https://github.com/shaneavila/sliding-puzzle-solver',
     },
